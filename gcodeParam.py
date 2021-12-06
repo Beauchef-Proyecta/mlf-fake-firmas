0de877795cedcd9088029cd98c71340d4685de2e
Rodrigo Navarro <navarrom.rodrigo@gmail.com>
2021-12-06 18:49:29 -0300

    Desarrollo y primer testeo script de parametrizacion de gcode

---
+++ gcodeParam.py
@@ -0,0 +1,44 @@
+import math
+import os
+class svgParam:
+    def __init__(self, gcode = '', home = (0,0,0), dim = 1):
+        self.gcode = gcode
+        self.home = home
+        self.x, self.y, self.z = home
+        self.dim = dim
+        self.ratio = 1 
+    
+    def parametrizar(self):   
+        def novem(n):       # funcion auxiliar para asegurar 6 digitos decimales
+            s = str(n)
+            dot = s.index('.') 
+            preDot, postDot = s[:dot+1], s[(dot+1):]
+            if len(postDot) <= 6:       # Si tiene 6 decimales o menos agergamos 0 a la cola hasta llegar a 6
+                while len(postDot) < 6:     # Al llegar a 6 sale del loop (si tiene 6 desde un inicio se lo salta)
+                    postDot += '0'
+                return preDot+postDot
+            else:       # Si tiene mas de 6 decimales se trunca al sexto
+                postDot = postDot[:6]
+                return preDot + postDot
+        with open(self.gcode, 'r+') as f:
+            newG = ''
+            G = f.readlines()
+            h = self.z
+            for line in G:
+                if line[:2] == 'G1' and 'X' in line and 'Y' in line:    # Instruccion de movimiento
+                    print(line)
+                    xpos, ypos = line.index('X'), line.index('Y')
+                    xval, yval = float(line[xpos+1:ypos-1]), float(line[ypos+1:len(line)-2])
+                    paramX = novem(self.x + self.ratio * xval)   # Se aseguran 6 decimales
+                    paramY = novem(self.y + self.ratio * yval)   # Se aseguran 6 decimales
+                    newG += line[0:xpos] + 'X{} '.format(paramX) + 'Y{} '.format(paramY) + 'Z{};'.format(h) + '\n'
+                else:
+                    newG += line
+            f.truncate(0)
+            f.seek(0)   # Cursos al inicio del archivo
+            f.write(newG)    # Sobreescribimos con el archivo modificado
+
+        
+                     
+
+
---
+++ testSVG.py
@@ -0,0 +1,13 @@
+from svg_to_gcode.svg_parser import parse_file
+from svg_to_gcode.compiler import Compiler, interfaces
+from gcodeParam import *
+
+gcode_compiler = Compiler(interfaces.Gcode, movement_speed=1000, cutting_speed=300, pass_depth=5)
+curves = parse_file("test2.svg") # Parse an svg file into geometric curves
+
+nombre = 'test4.gcode'
+gcode_compiler.append_curves(curves) 
+gcode_compiler.compile_to_file(nombre, passes=2)
+
+test = svgParam(gcode = nombre, home = (70000,70000,100))
+test.parametrizar()
