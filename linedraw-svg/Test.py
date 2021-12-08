b4231664f90451224e522df52b08aef61bfa547f
RaimundoLorca <71888726+RaimundoLorca@users.noreply.github.com>
2021-12-08 19:08:30 -0300

    repo svg

--- linedraw-svg/Test.py
+++ linedraw-svg/Test.py
@@ -5,4 +5,6 @@ lines = linedraw.sketch("images/Cristiano_Ronaldo_Signature_JPG.jpg")
 
 #lines = linedraw.sketch("images/Firma_Profe.jpeg")
 
+#lines = linedraw("images/Cristiano_Ronaldo_Signature_JPG.jpg")
+
 linedraw.visualize(lines) 
